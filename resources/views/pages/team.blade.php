3c780582617c8b69ef8c5e64ea356d4ecd7c2fa7
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-05-16 15:21:09 +0100

    fix

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -54,6 +54,10 @@ Team Members | Prime trast multiventures
                     <p>Senior Business Development Manager </p>
                     <p class="fw-bold mt-3">Awards</p>
                     <p>MTN Nigeria</p>
+                    <p class="fw-bold text-white mt-3">More</p>
+                    <p>Ahmed Buhari's journey began at age eleven, thrust into the role of a father figure after his father's passing. Despite early responsibilities, he earned a B.Sc. in Geology and embarked on a career in oil and gas in Lagos. </p>
+                    <p>His philanthropic efforts  through the Ahmed Bee Foundation support education and empower people with disabilities. Ahmed's achievements have earned him numerous accolades, including awards from MTN Nigeria and recognition from global organizations. He strives to unite Nigerian youth and promote political consciousness. </p>
+                    <p>Married with three children, Ahmed's story embodies resilience, growth, and generosity, reflecting his commitment to progress and innovation in Nigeria. For more insights into Ahmed's work, visit his YouTube page, Ahmed Bee Buhari.</p>
                 </div>
             </div>
         </div>
@@ -73,6 +77,13 @@ Team Members | Prime trast multiventures
                     <p class="fw-bold text-white mt-3">CEO</p>
                     <p>CEO of Prime trast Management and investment LTD</p>
                     <p>Board member in Skylar Inc</p>
+                    <p class="fw-bold text-white mt-3">More</p>
+                    <p>Oyinlade Adu was born 3rd of August 1979 to the Adu’s family of Obbo Aiyegunle in Ekiti Local government Area of Kwara State. </p>
+                    <p>He Graduated  from Federal University of Technology and  earned a B.Tech in Physics Computer Science. 
+                        He worked briefly as pioneer deployer of the Contributory Pension scheme with two different PFAs. </p>
+                    <p>His passion for contruction and creativity drove him into the Construction and facility management industry. </p>
+                    <p>Oyinlade is also the CEO of Prime trast Management and investment LTD and a board member in Skylar Inc. </p>
+                    <p>His philanthropic efforts  through The Future is Now youth Initiative, birth out of concern for youth participation in Leadership. He is married to a Beautiful Entrepreneur with two boys.</p>
                    </div>
             </div>
         </div>   
@@ -110,6 +121,28 @@ Team Members | Prime trast multiventures
                     <p>Registered Member of the Nigeria Society of Engineers (NSE), 2012</p>
                     <p>Registered Engineer with the Council For The Regulation of Engineering Practice in Nigeria (COREN), 2016</p>
                     <p>Registered Member of the Nigeria Institution of Civil Engineers (NICE), 2017</p>
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="col-sm-6">
+            <div class="content">
+                <div class="img"><img src="/img/img21.jpeg" alt=""></div>
+                <div class="text">
+                    <h5 class="text-white fw-bold">Tolulope Adebayo</h5>
+                    <h6 class="text-white fw-bold mb-3">Head, Business Strategy and Planning</h6>
+                    <p class="fw-bold text-white mt-3">Educations</p>
+                    <p class="">University of llorin - 2018</p>
+
+                    <p class="fw-bold text-white mt-3">Qualifications</p>
+                    <p>B.sc certificate in Surveying and Geoinformatics </p>
+                    <p>Worked at the Federal School of surveying Oyo State (Data Acquisition, Data Manipulation, Data Gathering, Processing and Storage)</p>
+                    <p>Worked in the telecommunication industry with MTN Nigeria</p>
+
+                    <p class="fw-bold text-white mt-3">More</p>
+                    <p>He is proficient at building and leading cross-functional teams with a focus on achieving operational excellence in business analysis, strategic planning, innovation and implementing enterprise-wide intelligence and performance management programs.</p>
+                    <p>His love for design and construction led him to join Prime Trast Multiventures Limited in the year 2020 where he has over the years worked in different capacity. He is currently the Head, Business Strategy and Planning, responsible for coordinating, developing and implementing strategies that satisfy the annual and long-term objectives of the Organization</p>
                 </div>
             </div>
         </div>
